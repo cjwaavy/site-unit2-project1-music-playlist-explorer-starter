25adb058ed26a69ef26c094e739c2158bf74f91b
Charles Hendrid McClasky <cmcclasky@meta.com>
2025-06-04 13:58:14 -0700

    v1

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,29 @@
 
+const dataResponse = fetch('data/data.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Failed to load data: ', error);
+    })
+
+const dialog = document.querySelector("dialog");
+const playlist_items = document.querySelectorAll(".playlist-card-item")
+const closeButton = document.querySelector(".closeModal")
+
+console.log(dialog)
+console.log(playlist_items)
+
+closeButton.addEventListener("click", () => {
+    dialog.close()
+    dialog.classList.remove("open-modal")
+})
+playlist_items.forEach((element) => {
+    element.addEventListener("click", () => {
+        dialog.classList.add("open-modal")
+        console.log("opening dialog")
+        dialog.showModal()
+    })
+})
+
